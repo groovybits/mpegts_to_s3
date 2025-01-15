ee0e2b3de8c6b60668d1df11e5030648ce71e0ae
Chris Kennedy <chris.kennedy@ltnglobal.com>
2025-01-15 08:02:35 -0500

    minio: Addr change

--- scripts/minio_server.sh
+++ scripts/minio_server.sh
@@ -2,10 +2,10 @@
 
 mkdir -p data
 
-podman stop s3minio
-podman rm s3minio
+podman stop s3minio 2>/dev/null
+podman rm s3minio 2>/dev/null
 
-podman run -p 127.0.0.1:9000:9000 -p :9001:9001 \
+podman run -p 127.0.0.1:9000:9000 -p 0.0.0.0:9001:9001 \
     --name s3minio \
     -e "MINIO_ROOT_USER=minioadmin" \
     -e "MINIO_ROOT_PASSWORD=minioadmin" \
